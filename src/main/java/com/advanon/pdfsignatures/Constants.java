c41406fe7a305e5ee2c54564b11b5153091dc925
Erk Struwe <estruwe@estruwe.de>
2021-04-23 12:19:46 +0200

    bump gradle version number

--- src/main/java/com/advanon/pdfsignatures/Constants.java
+++ src/main/java/com/advanon/pdfsignatures/Constants.java
@@ -3,7 +3,7 @@ package com.advanon.pdfsignatures;
 import com.itextpdf.text.pdf.PdfName;
 
 class Constants {
-  static final String VERSION = "0.0.2";
+  static final String VERSION = "0.1.0";
   static final PdfName SIGNATURE_FILTER = PdfName.ADOBE_PPKLITE;
   static final PdfName SIGNATURE_SUBFILTER = PdfName.ADBE_PKCS7_DETACHED;
   static final int DEFAULT_ESTIMATED_SIGNATURE_SIZE = 30000;
